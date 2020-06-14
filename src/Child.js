1ea355dbb15303c1520b37ec5aa22e15002aa9d2
umar51 <66181738+umar51@users.noreply.github.com>
2020-06-14 23:26:56 +0500

    updated

--- src/Child.js
+++ src/Child.js
@@ -1,11 +1,11 @@
 import React from 'react';
 //import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 function Child(props) {
   return (
     <div >
-      Child number{props.num}
+      Child number {props.num}
 
   
     </div>
